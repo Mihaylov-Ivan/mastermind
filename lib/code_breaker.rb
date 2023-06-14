88ff6e7cdc5bd56acd6eb1c8e4a3875dfe5b04ac
ivamih2k <mihaylov.ivan@siemens.com>
2023-06-14 17:04:31 +0100

    Cleanup code breaker

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -5,35 +5,28 @@ class CodeBreaker < Game
     include TextFormatting
     include Display
 
-    attr_reader :current_guess_formatted, :clues_formatted, :clues
     attr_reader :code
 
     def initialize
         @code = []
-        @current_guess = []
-        @current_guess_formatted = []
-        @clues = []
-        @clues_formatted = []
-        @correct_guess = false
         generate_code
         code_breaker_intro
     end
 
     def play
         turns = 0
-        until @correct_guess || turns == 12
+        until turns == 12
             prompt_guess(12 - turns)
-            @current_guess = get_code_input
-            @current_guess_formatted = format_code(@current_guess.clone)
-            @clues = create_clues(@code.clone, @current_guess)
-            @clues_formatted = format_clues(@clues.clone)
+            current_guess = get_code_input
+            current_guess_formatted = format_code(current_guess.clone)
+            clues = create_clues(code.clone, current_guess)
+            clues_formatted = format_clues(clues.clone)
             puts @code
-            show_guess_results(@current_guess_formatted, @clues_formatted)
-            @correct_guess = check_correct_guess(@clues)
-            turns+=1
-            puts code
+            show_guess_results(current_guess_formatted, clues_formatted)
+            break if check_correct_guess(clues)
+            turns += 1
         end
-        @correct_guess ? show_correct_guess(turns) : show_out_of_turns(format_code(code))
+        turns < 13 ? show_correct_guess(turns) : show_out_of_turns(format_code(code))
     end
 
     private 
