028b8585c766dfb83532b546f1f5d80ca3bd89f5
ivamih2k <mihaylov.ivan@siemens.com>
2023-06-14 14:46:56 +0100

    Add out of turn messages

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -22,26 +22,19 @@ class CodeMaker < Game
         numbers = ["1", "2", "3", "4", "5", "6"].shuffle!
         @guess_record = []
         four_numbers = find_four_numbers(numbers)
+        return game_over if turn > 12
         guess_perm = four_numbers.permutation.to_a.uniq
         reduce_permutations(guess_perm)
-        until turn == 13
+        until turn > 12
             sleep(1)
             show_computer_turn(turn)
             clues = make_a_turn(guess_perm[0])
             correct_guess = clues.all?("*")
-            break if correct_guess
+            # break if correct_guess
             reduce_permutations(guess_perm)
             @turn += 1
         end
-    end
-
-    def reduce_permutations(guess_perm)
-        guess_record.each do |guess|
-            guess_perm.each do |perm|
-                clues = create_clues(guess[0].clone, perm.clone)
-                guess_perm.reject! {|reject| reject == perm} unless guess[1] == clues.count('*') && guess[2] == clues.count('?')
-            end
-        end
+        game_over
     end
 
     def find_four_numbers(numbers, guess = [], index = 0)
@@ -50,6 +43,7 @@ class CodeMaker < Game
         guess.append(numbers[index]) until guess.length == 4
         clues = make_a_turn(guess)
         guess.pop(4-exact-same)
+        return if turn > 12
         @turn += 1
         find_four_numbers(numbers, guess, index + 1) unless clues.length == 4
         guess
@@ -62,5 +56,18 @@ class CodeMaker < Game
         guess_record.append([guess.clone, exact, same])
         show_guess_results(format_guess(guess), format_clues(clues))
         clues
+    end
+
+    def reduce_permutations(guess_perm)
+        guess_record.each do |guess|
+            guess_perm.each do |perm|
+                clues = create_clues(guess[0].clone, perm.clone)
+                guess_perm.reject! {|reject| reject == perm} unless guess[1] == clues.count('*') && guess[2] == clues.count('?')
+            end
+        end
+    end
+
+    def game_over
+        turn < 13 ? show_computer_correct_guess(turn) : show_computer_out_of_turns
     end
 end
